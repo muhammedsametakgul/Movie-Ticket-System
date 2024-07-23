c6c2d1e5d83e0b74ae74f3858fba7c1305e3da9e
Muhammed Samet Akgül <msametakgul@gmail.com>
2024-07-23 15:35:25 +0300

    Integrate liquibase to project

---
+++ movie-theater-ticket-booking/src/main/resources/db/changelog/changes/V1__addmovies.sql
@@ -0,0 +1,6 @@
+INSERT INTO movie_ticket_booking.movies (movie_name, duration, rating, release_date, genre, language) VALUES
+                                                                                                        ('The Matrix', 136, 8.7, '1999-03-31', 'ACTION', 'ENGLISH'),
+                                                                                                        ('Inception', 148, 8.8, '2010-07-16', 'ACTION', 'ENGLISH'),
+                                                                                                        ('GORA', 127, 7.8, '2004-11-12', 'ACTION', 'TURKISH'),
+                                                                                                        ('Interstellar', 169, 8.6, '2014-11-07', 'ACTION', 'ENGLISH'),
+                                                                                                        ('Dunkirk', 106, 7.9, '2017-07-21', 'ACTION', 'ENGLISH');
